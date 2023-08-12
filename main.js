c080be20bffe13d6ef53d9046daace6351d7238f
Esphalon <74604725+Esphalon@users.noreply.github.com>
2023-08-12 16:35:37 +0200

    Set small DOM cart text for feedback

---
+++ main.js
@@ -0,0 +1,16 @@
+const cartButton = document.querySelector(".add-cart");
+const cartButtonText = document.getElementById("cart-text");
+
+function addToCart(){
+  if(cartButtonText.innerHTML === 'Add to Cart'){
+    cartButtonText.innerHTML = 'Product Added';
+  } else{
+    cartButtonText.innerHTML = 'Add to Cart';
+  }
+}
+
+
+
+cartButton.addEventListener('click', addToCart);
+
+
